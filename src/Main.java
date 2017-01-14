0899f733dfc9c5790ba434f445535225c25cedae
Asif Mohaimen <saumik@csesociety.org>
2017-01-14 13:43:42 +0600

    Individual Char Reading complete

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 /**
  *
  * @author saumiko
  */
 public class Main {
-    public static void main(String args[])
+    public static void main(String args[]) throws FileNotFoundException, IOException
     {
-        System.out.println("Hello World");
+        FileInputStream fstream = new FileInputStream("input.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+        String Line;
+
+        while ((Line = br.readLine()) != null)   {
+            StringTokenizer inputLine = new StringTokenizer(Line);
+            while(inputLine.hasMoreTokens())
+            {
+                String word = inputLine.nextToken();
+                int wordLength = word.length();
+                for(int i=0;i<wordLength;i++)
+                {
+                    System.out.println(word.charAt(i));
+                }
+            }
+        }
+
+        br.close();
     }
 }
